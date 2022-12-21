378ac7db59cb1b1722d6efde068f0826955a56d1
AlifRizza <alif.11296@upi.edu>
2022-12-21 16:19:57 +0700

    nih yaa bang sudah di commit

--- frontend/src/views/Guru/editKelas.vue
+++ frontend/src/views/Guru/editKelas.vue
@@ -38,21 +38,19 @@
                         </div>
                         <div class="mt-3">
                             <label for="content">Content</label>
-                            <!-- <MDBTextarea rows="4" v-model="textareaValue" /> -->
-                            <QuillEditor v-model:content="dataKelas.editor" theme="snow"/>
+                            <MDBTextarea rows="5" v-model="dataKelas.editor" />
+                            <!-- <QuillEditor v-model:content="dataKelas.editor" theme="snow"/> -->
                         </div>
                         <div class="mt-3">
                             <label for="kategori-kelas">Kategori Modul</label>
                             <select v-model="dataKelas.kategoriModul" class="form-control md-2">
-                                <option>A</option>
-                                <option>B</option>
-                                <option>C</option>
+                                <option v-for="i in testing" :key="i.id" :value="i.id">{{ i.judul }}</option>
                             </select>
                         </div>
-                        <div class="d-grid mt-3 mb-5 align-self-center w-25">
+                        <!-- <div class="d-grid mt-3 mb-5 align-self-center w-25">
                         <button type="submit" name="tambah-soal" class="btn btn-block grey red-maroon-text"
                             style="background-color: #901311; text-transform: none;">Tambah Soal</button>
-                        </div>
+                        </div> -->
                     </div>
                 </MDBTabPane>
                 <MDBTabPane tabId="ex1-3">
@@ -60,9 +58,7 @@
                         <div class="mt-3">
                             <label for="kategori-modul">Kategori Modul</label>
                             <select v-model="dataSoal.kategoriModul" class="form-control md-2">
-                                <option>ABC</option>
-                                <option>ABC</option>
-                                <option>ABC</option>
+                                <option v-for="i in testing" :key="i.id" :value="i.id">{{ i.judul }}</option>
                             </select>
                         </div>
                         <div class="mt-3">
@@ -72,25 +68,25 @@
                         <div class="mt-5 d-flex flex-sm-row justify-content-between">
                             <div>
                                 <div class="row mb-3 justify-content-center">
-                                    <label for="soal" class="col-sm-2 col-form-label">A</label>
+                                    <label for="soal" class="col-sm-2 col-form-label">1</label>
                                     <div class="col-sm-10">
                                         <input v-model="dataSoal.opsiA" name="soal" class="form-control form-control-sm" id="soal" required>
                                     </div>
                                 </div>
                                 <div class="row mb-3">
-                                    <label for="soal" class="col-sm-2 col-form-label">B</label>
+                                    <label for="soal" class="col-sm-2 col-form-label">2</label>
                                     <div class="col-sm-10">
                                         <input v-model="dataSoal.opsiB" name="soal" class="form-control form-control-sm" id="soal" required>
                                     </div>
                                 </div>
                                 <div class="row mb-3">
-                                    <label for="soal" class="col-sm-2 col-form-label">C</label>
+                                    <label for="soal" class="col-sm-2 col-form-label">3</label>
                                     <div class="col-sm-10">
                                         <input v-model="dataSoal.opsiC" name="soal" class="form-control form-control-sm" id="soal" required>
                                     </div>
                                 </div>
                                 <div class="row mb-3">
-                                    <label for="soal" class="col-sm-2 col-form-label">D</label>
+                                    <label for="soal" class="col-sm-2 col-form-label">4</label>
                                     <div class="col-sm-10">
                                         <input v-model="dataSoal.opsiD" name="soal" class="form-control form-control-sm" id="soal" required>
                                     </div>
@@ -99,24 +95,24 @@
                             <div>
                                 <label for="opsi-benar">Opsi Benar</label>
                                 <select v-model="dataSoal.opsiBenar" class="form-control md-2">
-                                    <option>A</option>
-                                    <option>B</option>
-                                    <option>C</option>
-                                    <option>D</option>
+                                    <option>1</option>
+                                    <option>2</option>
+                                    <option>3</option>
+                                    <option>4</option>
                                 </select>
                             </div>
                         </div>
-                        <div class="d-grid mt-3 mb-5 align-self-center w-25">
-                        <button type="submit" name="tambah-soal" class="btn btn-block grey red-maroon-text"
+                        <!-- <div class="d-grid mt-3 mb-5 align-self-center w-25">
+                        <button @click="editdataKelas" type="submit" name="tambah-soal" class="btn btn-block grey red-maroon-text"
                             style="background-color: #901311; text-transform: none;">Tambah Soal</button>
-                        </div>
+                        </div> -->
                     </div>
                 </MDBTabPane>
                 </MDBTabContent>
                 <!-- Tabs content -->
             </MDBTabs>
             <div class="d-grid mt-5 mb-3 align-self-center w-25">
-                <button @click="editdataKelas" type="submit" name="upload" class="btn btn-block text-light"
+                <button @click="postData" type="submit" name="upload" class="btn btn-block text-light"
                  style="background-color: #901311; text-transform: none;">Upload</button>
             </div>
         </div>
@@ -124,8 +120,8 @@
 </template>
 
 <script>
-import { QuillEditor } from '@vueup/vue-quill'
-import '@vueup/vue-quill/dist/vue-quill.snow.css';
+// import { QuillEditor } from '@vueup/vue-quill'
+// import '@vueup/vue-quill/dist/vue-quill.snow.css';
 import axios from 'axios'
 
 
@@ -148,20 +144,24 @@ export default {
       MDBTabItem,
       MDBTabPane,
       MDBTextarea,
-      QuillEditor
+    //   QuillEditor
     //   MDBRadio,
     },
     data() {
         return {
             profileLogo: require('@/assets/Profile.png'),
             editLogo: require('@/assets/Edit.png'),
+            testing: [],
+            idMod:'',
             dataKelas: {
                 judul: '',
                 kelas: '',
                 deskripsi: '',
                 judulSubmodul: '',
                 kategoriModul: '',
-                editor: ''
+                editor: '',
+                background: 'bg.com',
+                id_guru: 1
             },
             dataSoal: {
                 kategoriModul: '',
@@ -174,17 +174,63 @@ export default {
             }
         }
     },
-    methods:{
-        async editdataKelas(){
-            try {
-                const response = await axios.post('http://localhost:8080/api/course/create', {
-                    "kelas":this.dataKelas.kelas
-                })
-                console.log(response.data)
-            } catch (error) {
-                console.error(error)
-            }
-        }
-    }
+    // methods:{
+    //     async editdataKelas(){
+    //         try {
+    //             const response = await axios.post('http://localhost:8081/api/course', {
+    //                 kelas:this.dataKelas.kelas
+    //             })
+    //             console.log(response.data)
+    //         } catch (error) {
+    //             console.error(error)
+    //         }
+    //     }
+    // },
+    async created() {
+        const response1 = await axios.get('http://localhost:8081/api/moduls');
+        // const response2 = await axios.get(`http://localhost:8081/api/siswa/detail/${idSiswa}/nilai`);
+        this.testing = response1.data.data;
+        // this.siswaDetail = response2.data.data;
+    },
+    methods: {
+        async postData() {
+            await axios.post('http://localhost:8081/api/modul/create', {
+                judul:this.dataKelas.judul,
+                deskripsi:this.dataKelas.deskripsi,
+                kelas:this.dataKelas.kelas
+            });
+            await axios.post('http://localhost:8081/api/modul/sub/create', {
+                nama:this.dataKelas.judulSubmodul,
+                materi:[{
+                    nama: 'Judul',
+                    materi:this.dataKelas.editor}],
+                id_modul:this.dataKelas.kategoriModul
+            });
+            await axios.post('http://localhost:8081/api/soal/create', {
+                soal:this.dataSoal.soal,
+                jawaban:this.dataSoal.opsiBenar,
+                pilihan:[
+                    {
+                        idPilihan:1,
+                        pilihan: this.dataSoal.opsiA
+                    },
+                    {
+                        idPilihan:2,
+                        pilihan: this.dataSoal.opsiB
+                    },
+                    {
+                        idPilihan:3,
+                        pilihan: this.dataSoal.opsiC
+                    },
+                    {
+                        idPilihan:4,
+                        pilihan: this.dataSoal.opsiD
+                    },
+                ],
+                idModul: this.dataKelas.kategoriModul
+            });
+        },
+    },
+
 }
 </script>
